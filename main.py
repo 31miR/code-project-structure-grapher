21319cc61cc0f8d2c221d5452e9a25826ebe92ec
31miR <76799355+31miR@users.noreply.github.com>
2023-12-28 00:37:36 +0100

    implement simple program that prints nodes and it's children

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import cProjectStructure
+
 #main guard in case this program is also [for some weird reason] used as a library
 def main():
+    print("Please provide a relative path to the folder you want to analyze")
+    print("You can use . and .. in the path string")
+    rel_path = str(input('path:'))
+    nodes = cProjectStructure.getFileNodesFromFolder(rel_path)
+    cProjectStructure.setInitialChildren(nodes)
+    for i in nodes:
+        print(i)
     return 0
 
 if __name__ == '__main__':
