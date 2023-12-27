22da99afa1124c60889b97f2d91946e4f3c77e47
31miR <76799355+31miR@users.noreply.github.com>
2023-12-28 00:36:51 +0100

    implement string conversions for File and FileNode

--- fileClass.py
+++ fileClass.py
@@ -7,9 +7,13 @@ class File:
         if (len(parts) < 2):
             return "NO EXTENSION"
         return parts[-1]
+    def __str__(self):
+        return self.name
 
 class FileNode:
     def __init__(self, currentFile, children, isExternal = False):
         self.file = currentFile
         self.children = children
         self.isExternal = isExternal
+    def __str__(self):
+        return "{node name: " + self.file.name + "with children: " + str([i.file.name for i in self.children]) + "}"
