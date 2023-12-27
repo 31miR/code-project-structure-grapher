12924abc5b0f50843fe39295ff28146e1e053895
31miR <76799355+31miR@users.noreply.github.com>
2023-12-27 23:45:02 +0100

    implement function that looks for all #include file names

--- cProjectStructure.py
+++ cProjectStructure.py
@@ -12,3 +12,23 @@ def getFileNodesFromFolder(pathToFolder):
                 result.append(fileClass.FileNode(file, []))
     return result
 
+def getChildNamesFromFile(file_path):
+    result = []
+    file = open(file_path)
+    for line in file:
+        if line[0:8] == "#include":
+            indexStart = -1
+            indexEnd = -1
+            if '<' in line:
+                indexStart = line.index('<') + 1
+                indexEnd = line.index('>')
+            elif '"' in line:
+                indexStart = line.index('"') + 1
+                indexEnd = indexStart + line[indexStart:].index('"')
+            else:
+                continue
+            path = line[indexStart : indexEnd]
+            path = path.split('/')
+            result.append(path[-1])
+    file.close()
+    return result
